fdf45b57283455d63dd4401951a56895f86ea024
Luis Samiento <lsarmientom010@gmail.com>
2020-06-22 09:13:56 -0500

    desplegar a heroku

--- server/server.js
+++ server/server.js
@@ -6,11 +6,12 @@ const methodOverride = require('method-override');
 // init
 const app = express();
 
+const port = process.env.PORT || 3000;
 // Static files
 app.use(express.static(path.join(__dirname, '../public')));
 
 // settings
-app.listen(3000, () => console.log('online', new Date().getHours(), new Date().getMinutes(), new Date().getSeconds()));
+app.listen(port, () => console.log('online', new Date().getHours(), new Date().getMinutes(), new Date().getSeconds()));
 
 app.set('views', path.join(__dirname, '../views'));
 app.engine(
